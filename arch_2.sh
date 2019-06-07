d34a30bd06c4514547ca7211133be2aa43459d11
McDAC <47005868+McDAC@users.noreply.github.com>
2019-06-07 16:59:38 +0300

    Update arch_2.sh

--- arch_2.sh
+++ arch_2.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-echo 'Прписываем имя компьютера'
-echo "McDAC" > /etc/hostname
+read -p "Введите имя компьютера: " hostname
+read -p "Введите имя пользователя: " username
+
+echo 'Прописываем имя компьютера'
+echo $hostname > /etc/hostname
 ln -svf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 
 echo '3.4 Добавляем русскую локаль системы'
@@ -20,22 +23,26 @@ echo 'FONT=cyr-sun16' >> /etc/vconsole.conf
 echo 'Создадим загрузочный RAM диск'
 mkinitcpio -p linux
 
-echo 'Создаем root пароль'
-passwd
-
 echo '3.5 Устанавливаем загрузчик'
 pacman -Syy
 pacman -S grub --noconfirm 
 grub-install /dev/sda
 
 echo 'Обновляем grub.cfg'
-grub-mkconfig -o /boot/grub/grub.cfg 
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo 'Ставим программу для Wi-fi'
+pacman -S dialog wpa_supplicant --noconfirm 
 
 echo 'Добавляем пользователя'
-useradd -m -g users -G wheel -s /bin/bash valery
+useradd -m -g users -G wheel -s /bin/bash $username
+
+echo 'Создаем root пароль'
+passwd
 
 echo 'Устанавливаем пароль пользователя'
-passwd valery
+passwd $username
+
 echo 'Устанавливаем SUDO'
 echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
 
@@ -53,24 +60,38 @@ elif [[ $vm_setting == 1 ]]; then
 fi
 
 echo 'Ставим иксы и драйвера'
-pacman -S $gui_install --noconfirm
+pacman -S $gui_install
 
-echo 'Ставим Xfce, LXDM и сеть'
-pacman -S xfce4 xfce4-goodies lxdm networkmanager network-manager-applet ppp --noconfirm
+echo "Какое DE ставим?"
+read -p "1 - XFCE, 2 - KDE, 3 - Openbox: " vm_setting
+if [[ $vm_setting == 1 ]]; then
+  pacman -S xfce4 xfce4-goodies --noconfirm
+elif [[ $vm_setting == 2 ]]; then
+  pacman -Sy plasma-meta kdebase --noconfirm
+elif [[ $vm_setting == 3 ]]; then  
+  pacman -S  openbox xfce4-terminal
+fi
+
+echo 'Какой ставим DM ?'
+read -p "1 - sddm (Для Openbox не ставить, нет выбора пользователя), 2 - lxdm: " dm_setting
+if [[ $dm_setting == 1 ]]; then
+  pacman -Sy sddm sddm-kcm --noconfirm
+  systemctl enable sddm.service -f
+elif [[ $dm_setting == 2 ]]; then
+  pacman -S lxdm --noconfirm
+  systemctl enable lxdm
+fi
 
 echo 'Ставим шрифты'
 pacman -S ttf-liberation ttf-dejavu --noconfirm 
 
+echo 'Ставим сеть'
+pacman -S networkmanager network-manager-applet ppp --noconfirm
+
 echo 'Подключаем автозагрузку менеджера входа и интернет'
-systemctl enable lxdm NetworkManager
+systemctl enable NetworkManager
 
-echo 'Ставим wget'
-pacman -S wget --noconfirm 
-
-echo 'Сделайте exit и reboot. После перезагрузки заходим под пользователем'
-echo 'После чего скачайте и запустите скрипт, который установит программы, настройки XFCE и темы:'
-echo 'wget ordanax.ru/arch/arch_3.sh'
-echo 'sh arch_3.sh'
+echo 'Установка завершена! Перезагрузите систему.'
+echo 'Если хотите подключить AUR, установить мои конфиги XFCE, тогда после перезагрзки и входа в систему, установите wget (sudo pacman -S wget) и выполните команду:'
+echo 'wget git.io/arch3.sh && sh arch3.sh'
 exit
-read -p "Пауза 3 ceк." -t 3
-reboot
