e973b7539e8eabb21eb71a3229ea1ee697f00f0a
McDAC <47005868+McDAC@users.noreply.github.com>
2019-08-29 21:28:43 +0300

    Update 1arch.sh

--- 1arch.sh
+++ 1arch.sh
@@ -25,6 +25,12 @@ echo '2.4 создание разделов'
   echo +40G;
 
   echo n;
+  echo;
+  echo;
+  echo;
+  echo +4096M;
+
+  echo n;
   echo p;
   echo;
   echo;
@@ -38,15 +44,17 @@ echo 'Ваша разметка диска'
 fdisk -l
 
 echo '2.4.2 Форматирование дисков'
-mkfs.ext2  /dev/sda1 -L Boot
-mkfs.ext4  /dev/sda2 -L Root
-mkfs.ext4  /dev/sda3 -L Home
+mkfs.ext2  /dev/sda1 -L boot
+mkfs.ext4  /dev/sda2 -L root
+mkswap /dev/sda3 -L swap
+mkfs.ext4  /dev/sda4 -L home
 
 echo '2.4.3 Монтирование дисков'
 mount /dev/sda2 /mnt
 mkdir /mnt/{boot,home}
 mount /dev/sda1 /mnt/boot
-mount /dev/sda3 /mnt/home
+swapon /dev/sda3
+mount /dev/sda4 /mnt/home
 
 echo '3.1 Выбор зеркал для загрузки. Ставим зеркало от Яндекс'
 echo "Server = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
@@ -57,4 +65,4 @@ pacstrap /mnt base base-devel
 echo '3.3 Настройка системы'
 genfstab -pU /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt
+arch-chroot /mnt 
